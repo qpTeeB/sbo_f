d3cafef04591a3d3682f9b6a848d27ac46607d4d
qpTeeB <85889759+qpTeeB@users.noreply.github.com>
2022-12-27 21:03:15 +0300

    Добавлен класс формы главной страницы

---
+++ MainPage.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace SBO_f
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.LKbutton = new System.Windows.Forms.Button();
+            this.createOrdButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(107)))), ((int)(((byte)(216)))));
+            this.panel1.Controls.Add(this.closeButton);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.helpButton);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.LKbutton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(908, 57);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseMove);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.AutoSize = true;
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.closeButton.ForeColor = System.Drawing.Color.White;
+            this.closeButton.Location = new System.Drawing.Point(868, 15);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(28, 26);
+            this.closeButton.TabIndex = 5;
+            this.closeButton.Text = "X";
+            this.closeButton.Click += new System.EventHandler(this.CloseButton_Click);
+            this.closeButton.MouseEnter += new System.EventHandler(this.CloseButton_MouseEnter);
+            this.closeButton.MouseLeave += new System.EventHandler(this.CloseButton_MouseLeave);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Oswald", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(151, -2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 57);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Главная";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // helpButton
+            // 
+            this.helpButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.helpButton.FlatAppearance.BorderSize = 0;
+            this.helpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.helpButton.Font = new System.Drawing.Font("Oswald", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.helpButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.helpButton.Location = new System.Drawing.Point(64, -2);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(90, 57);
+            this.helpButton.TabIndex = 1;
+            this.helpButton.Text = "Help";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // LKbutton
+            // 
+            this.LKbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LKbutton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LKbutton.FlatAppearance.BorderSize = 0;
+            this.LKbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LKbutton.Font = new System.Drawing.Font("Oswald", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LKbutton.ForeColor = System.Drawing.Color.White;
+            this.LKbutton.Location = new System.Drawing.Point(658, 3);
+            this.LKbutton.Name = "LKbutton";
+            this.LKbutton.Size = new System.Drawing.Size(204, 54);
+            this.LKbutton.TabIndex = 2;
+            this.LKbutton.Text = "Личный кабинет";
+            this.LKbutton.UseVisualStyleBackColor = true;
+            this.LKbutton.Click += new System.EventHandler(this.LKbutton_Click);
+            // 
+            // createOrdButton
+            // 
+            this.createOrdButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(107)))), ((int)(((byte)(216)))));
+            this.createOrdButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.createOrdButton.FlatAppearance.BorderSize = 0;
+            this.createOrdButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createOrdButton.Font = new System.Drawing.Font("Oswald", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.createOrdButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.createOrdButton.Location = new System.Drawing.Point(64, 511);
+            this.createOrdButton.Name = "createOrdButton";
+            this.createOrdButton.Size = new System.Drawing.Size(157, 36);
+            this.createOrdButton.TabIndex = 2;
+            this.createOrdButton.Text = "Добавить предложение";
+            this.createOrdButton.UseVisualStyleBackColor = false;
+            this.createOrdButton.Click += new System.EventHandler(this.createOrdButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listView1.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.ForeColor = System.Drawing.Color.White;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(64, 149);
+            this.listView1.Name = "listView1";
+            this.listView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listView1.Size = new System.Drawing.Size(780, 345);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Oswald", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(55, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 49);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Последние объявления";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SBO_f.Properties.Resources.icon;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 57);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
+            this.ClientSize = new System.Drawing.Size(908, 574);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.createOrdButton);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainPage_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button helpButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button LKbutton;
+        private System.Windows.Forms.Button createOrdButton;
+        private System.Windows.Forms.Label closeButton;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ MainPage.cs
@@ -0,0 +1,121 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SBO_f
+{
+    public partial class MainPage : Form
+    {
+        private string loginUser;
+
+        public string LoginUser 
+        { 
+            get 
+            { 
+                return loginUser; 
+            } 
+            set 
+            { 
+                loginUser = value; 
+            } 
+        }
+        public MainPage(string login)
+        {
+            LoginUser = login;
+            InitializeComponent();
+        }
+        Point point;
+        private void MainPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - point.X;
+                this.Top += e.Y - point.Y;
+            }
+        }
+
+        private void MainPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            point = new Point(e.X, e.Y);
+        }
+        private void CloseButton_MouseEnter(object sender, EventArgs e)
+        {
+            closeButton.ForeColor = Color.Gray;
+        }
+
+        private void CloseButton_MouseLeave(object sender, EventArgs e)
+        {
+            closeButton.ForeColor = Color.White;
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+            
+            DataBase db = new DataBase();
+            
+            db.openConnection();
+            
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `orders`", db.getConnection());
+            
+            MySqlDataReader reader = command.ExecuteReader();
+            
+            List<string[]> data = new List<string[]>();
+
+            while(reader.Read())
+            {
+                data.Add(new string[4]);
+                for(int i = 0; i < 4; i++)
+                {
+                    data[data.Count - 1][i] = reader[i].ToString();
+                }
+            }
+
+            reader.Close();
+
+            db.closeConnection();
+
+
+            for (int i = data.Count; i > 0;i--)
+            {
+                ListViewGroup group = new ListViewGroup(data[i - 1][0]);
+                listView1.Items.Add(new ListViewItem(data[i - 1][1], group));
+                listView1.Items.Add(new ListViewItem(data[i - 1][2], group));
+                listView1.Items.Add(new ListViewItem("Контактный номер: +" + data[i - 1][3], group));
+                listView1.Groups.Add(group);
+            }           
+        }
+
+        private void LKbutton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormLK lkPage = new FormLK(loginUser);
+            lkPage.Show();
+        }
+
+        private void createOrdButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TradeForm tradeForm = new TradeForm(loginUser);
+            tradeForm.Show();
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            HelpForm help = new HelpForm(LoginUser);
+            help.Show();
+        }
+    }
+}
