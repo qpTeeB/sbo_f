b0ac86f5eacdf3b9f949b7cd896a17b518c6bc03
qpTeeB <85889759+qpTeeB@users.noreply.github.com>
2022-12-27 21:07:14 +0300

    Добавлен класс формы создания объявления

---
+++ TradeForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace SBO_f
+{
+    partial class TradeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.phone_textbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.wtb_textbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.wts_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.LKbutton = new System.Windows.Forms.Button();
+            this.returnToMain_button = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
+            this.panel1.Controls.Add(this.CreateButton);
+            this.panel1.Controls.Add(this.phone_textbox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.wtb_textbox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.wts_textbox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 588);
+            this.panel1.TabIndex = 1;
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(107)))), ((int)(((byte)(216)))));
+            this.CreateButton.FlatAppearance.BorderSize = 0;
+            this.CreateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CreateButton.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CreateButton.ForeColor = System.Drawing.Color.White;
+            this.CreateButton.Location = new System.Drawing.Point(568, 435);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(148, 47);
+            this.CreateButton.TabIndex = 7;
+            this.CreateButton.Text = "Создать объявление";
+            this.CreateButton.UseVisualStyleBackColor = false;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
+            // 
+            // phone_textbox
+            // 
+            this.phone_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.phone_textbox.Location = new System.Drawing.Point(63, 462);
+            this.phone_textbox.Name = "phone_textbox";
+            this.phone_textbox.Size = new System.Drawing.Size(280, 26);
+            this.phone_textbox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Oswald", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(54, 410);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(299, 49);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Укажите номер телефона:";
+            // 
+            // wtb_textbox
+            // 
+            this.wtb_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.wtb_textbox.Location = new System.Drawing.Point(63, 291);
+            this.wtb_textbox.Multiline = true;
+            this.wtb_textbox.Name = "wtb_textbox";
+            this.wtb_textbox.Size = new System.Drawing.Size(653, 83);
+            this.wtb_textbox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Oswald", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(54, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(471, 49);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Опишите товар, коотрый вы хотите найти:";
+            // 
+            // wts_textbox
+            // 
+            this.wts_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.wts_textbox.Location = new System.Drawing.Point(63, 134);
+            this.wts_textbox.Multiline = true;
+            this.wts_textbox.Name = "wts_textbox";
+            this.wts_textbox.Size = new System.Drawing.Size(653, 83);
+            this.wts_textbox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Oswald", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(54, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 49);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Добавьте описание к вашему товару:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(107)))), ((int)(((byte)(216)))));
+            this.panel2.Controls.Add(this.LKbutton);
+            this.panel2.Controls.Add(this.returnToMain_button);
+            this.panel2.Controls.Add(this.CloseButton);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 57);
+            this.panel2.TabIndex = 0;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            // 
+            // LKbutton
+            // 
+            this.LKbutton.FlatAppearance.BorderSize = 0;
+            this.LKbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LKbutton.Font = new System.Drawing.Font("Oswald", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LKbutton.ForeColor = System.Drawing.Color.White;
+            this.LKbutton.Location = new System.Drawing.Point(476, -1);
+            this.LKbutton.Name = "LKbutton";
+            this.LKbutton.Size = new System.Drawing.Size(199, 57);
+            this.LKbutton.TabIndex = 4;
+            this.LKbutton.Text = "Личный кабинет";
+            this.LKbutton.UseVisualStyleBackColor = true;
+            this.LKbutton.Click += new System.EventHandler(this.LKbutton_Click);
+            // 
+            // returnToMain_button
+            // 
+            this.returnToMain_button.FlatAppearance.BorderSize = 0;
+            this.returnToMain_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.returnToMain_button.Font = new System.Drawing.Font("Oswald", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.returnToMain_button.ForeColor = System.Drawing.Color.White;
+            this.returnToMain_button.Location = new System.Drawing.Point(161, 0);
+            this.returnToMain_button.Name = "returnToMain_button";
+            this.returnToMain_button.Size = new System.Drawing.Size(120, 57);
+            this.returnToMain_button.TabIndex = 3;
+            this.returnToMain_button.Text = "Главная";
+            this.returnToMain_button.UseVisualStyleBackColor = true;
+            this.returnToMain_button.Click += new System.EventHandler(this.returnToMain_button_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CloseButton.ForeColor = System.Drawing.Color.White;
+            this.CloseButton.Location = new System.Drawing.Point(737, -1);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(63, 58);
+            this.CloseButton.TabIndex = 2;
+            this.CloseButton.Text = "X";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Oswald", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(63, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Help";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SBO_f.Properties.Resources.icon;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 57);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TradeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 588);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TradeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TradeForm";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TradeForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TradeForm_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button LKbutton;
+        private System.Windows.Forms.Button returnToMain_button;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox wtb_textbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox wts_textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox phone_textbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button CreateButton;
+    }
+}
